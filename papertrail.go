4fdb2e82c44cc6aa77e57c996be12624b71d79de
Marc Gauthier <marc.gauthier3@gmail.com>
2022-10-29 11:17:40 -0400

    Update papertrail.go

--- papertrail.go
+++ papertrail.go
@@ -1,72 +1,84 @@
 package papertrail
 
 import (
-	"encoding/json"
 	"fmt"
-	"net"
+	"log"
+	"log/syslog"
 	"runtime"
-	"strings"
 	"time"
+	"strings"
 )
-
-type Msg struct {
-	App   string
-	Time  int64
-	Msg   string
-	Level int
-}
 
 var debug bool = false
 
-var host, application string
+var w *syslog.Writer
 
-func Init(Host, Applicationname string, _debug bool) {
-	host = Host
-	application = Applicationname
-	debug = _debug
+func Init(Host, Applicationname string) {
+	var err error
+	w, err = syslog.Dial("udp", Host, syslog.LOG_EMERG|syslog.LOG_DAEMON, Applicationname)
+	if err != nil {
+		log.Fatal("failed to dial syslog")
+	}
 }
 
-func send(msg string, level int) {
-	m := Msg{App: application, Time: time.Now().UnixNano(), Msg: msg, Level: level}
-	buf, _ := json.Marshal(m)
-
-	conn, err := net.Dial("udp", host)
-	if err != nil {
-		fmt.Printf("Some error %v", err)
-		return
-	}
-	fmt.Fprintf(conn, string(buf))
+func console(s string) {
+	fmt.Println(time + " " + s)
 }
 
 // Info example:
 //
 // Info("timezone %s", timezone)
+//
 func Info(msg string, vars ...interface{}) {
-	send(fmt.Sprintf(strings.Join([]string{"[INFO ]", msg}, " "), vars...), 0)
+	s := fmt.Sprintf(strings.Join([]string{"[INFO ]", msg}, " "), vars...)
+	w.Info(s)
+	console(s)
+	
+}
+
+// Debug example:
+//
+// Debug("timezone %s", timezone)
+//
+func Debug(msg string, vars ...interface{}) {
+	if debug {
+		s := fmt.Sprintf(strings.Join([]string{"[DEBUG]", msg}, " "), vars...)
+		w.Debug(s)
+		console(s)
+	}
 }
 
 // Fatal example:
 //
 // Fatal(errors.New("db timezone must be UTC"))
+//
 func Fatal(err error) {
 	pc, fn, line, _ := runtime.Caller(1)
 	// Include function name if debugging
 	if debug {
-		send(fmt.Sprintf("[FATAL] %s [%s:%s:%d]", err, runtime.FuncForPC(pc).Name(), fn, line), 4)
+		s := fmt.Sprintf("[FATAL] %s [%s:%s:%d]", err, runtime.FuncForPC(pc).Name(),fn, line)
+		w.Alert(s)
 	} else {
-		send(fmt.Sprintf("[FATAL] %s [%s:%d]", err, fn, line), 4)
+		s := fmt.Sprintf("[FATAL] %s [%s:%d]", err, fn, line)
+		w.Alert(s)
+		console(s)
 	}
 }
 
 // Error example:
 //
 // Error(errors.Errorf("Invalid timezone %s", timezone))
+//
 func Error(err error) {
 	pc, fn, line, _ := runtime.Caller(1)
 	// Include function name if debugging
 	if debug {
-		send(fmt.Sprintf("[ERROR] %s [%s:%s:%d]", err, runtime.FuncForPC(pc).Name(), fn, line), 3)
+		s := fmt.Sprintf("[ERROR] %s [%s:%s:%d]", err, runtime.FuncForPC(pc).Name(), fn, line)
+		w.Err(s)
+		console(s)
 	} else {
-		send(fmt.Sprintf("[ERROR] %s [%s:%d]", err, fn, line), 3)
+		s:= fmt.Sprintf("[ERROR] %s [%s:%d]", err, fn, line)
+		w.Err(s)
+		console(s)
 	}
 }
